7fc98cbd153ffba3cd80da0210b69b1d5b60f097
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2023-09-24 17:12:32 -0300

    Did some simple updates

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,4 @@
-var vel = 61
+var vel = 63
 console.log(`A velocidade do seu carro é ${vel}Km/h.`)
 if (vel > 60) { //Condição simples
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
